37bba8f2f974a165cfa8495602ce55f259ce3d27
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2024-03-03 08:39:24 +0000

    Rebrand, refit.

--- main.ps1
+++ main.ps1
@@ -4,28 +4,8 @@ function Show-Divider {
 
 function Show-Header {
     Clear-Host
-    Write-Host "`n=================( TimeLoop )=================`n"
+    Write-Host "`n=================( TimeBars )=================`n"
 }
-
-function Show-TimerOverScreen {
-    param([TimeSpan]$Duration)
-
-    Show-Header
-    Write-Host "`n`n`n`n`n`n`n                 Timer Over!`n`n`n`n`n`n`n"
-    Show-Divider
-    # Play sound
-    $player = New-Object System.Media.SoundPlayer ".\sounds\alarm-bleep.wav"
-    $player.PlaySync()
-
-    $action = Read-Host "Select, Repeat = R, Menu = M, Exit = X"
-    switch ($action) {
-        "R" { Start-Timer -Duration $Duration }
-        "M" { Show-Menu }
-        "X" { return }
-        default { Write-Host "Invalid option, please try again."; Start-Sleep -Seconds 2; Show-TimerOverScreen -Duration $Duration }
-    }
-}
-
 
 function Start-Timer {
     param([TimeSpan]$Duration)
@@ -41,8 +21,8 @@ function Start-Timer {
         $currentOperation = "Elapsed: $($elapsed.ToString('hh\:mm\:ss')) - Remaining: $($remaining.ToString('hh\:mm\:ss'))"
         $progressBar = '[' + '█' * [Math]::Round($percentComplete / 2.5) + ' ' * (40 - [Math]::Round($percentComplete / 2.5)) + ']'
         Write-Host "$status`n"
-        Write-Host "`n`n`n`n   $currentOperation`n"
-        Write-Host "  $progressBar`n`n`n`n`n`n"
+        Write-Host "`n`n`n   $currentOperation`n"
+        Write-Host "  $progressBar`n`n`n`n`n"
 		Show-Divider
         Start-Sleep -Seconds 5
     }
@@ -50,17 +30,35 @@ function Start-Timer {
     Show-TimerOverScreen -Duration $Duration
 }
 
+function Show-TimerOverScreen {
+    param([TimeSpan]$Duration)
+
+    Show-Header
+    Write-Host "`n`n`n`n`n`n                 Timer Over!`n`n`n`n`n`n"
+    Show-Divider
+    # Play sound
+    $player = New-Object System.Media.SoundPlayer ".\sounds\alarm-bleep.wav"
+    $player.PlaySync()
+
+    $action = Read-Host "Select, Repeat = R, Menu = M, Exit = X"
+    switch ($action) {
+        "R" { Start-Timer -Duration $Duration }
+        "M" { Show-Menu }
+        "X" { return }
+        default { Write-Host "Invalid option, please try again."; Start-Sleep -Seconds 2; Show-TimerOverScreen -Duration $Duration }
+    }
+}
 
 function Show-Menu {
     do {
         Show-Header
-        Write-Host "`n             1. 2 Hours Timer`n"
+        Write-Host "             1. 2 Hours Timer`n"
         Write-Host "             2. 1 Hour Timer`n"
         Write-Host "             3. 30 Minutes Timer`n"
         Write-Host "             4. 15 Minutes Timer`n"
         Write-Host "             5. 10 Minutes Timer`n"
         Write-Host "             6. 5 Minutes Timer`n"
-		Write-Host "             7. 1 Minute Timer`n"
+		Write-Host "             7. 1 Minute Timer"
         Show-Divider
         $choice = Read-Host "Select, Options = 1-7, Exit = X"
         switch ($choice) {
